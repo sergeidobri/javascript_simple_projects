eeb396a7cccbe8b0a50c73ac6b57aaa3ac95b2e6
Sergey <teplichkinteplichkinov@gmail.com>
2025-09-12 13:54:45 +0300

    Client states homework was updated

--- client-state/popup/app.js
+++ client-state/popup/app.js
@@ -1,10 +1,13 @@
 const popup = document.querySelector(".modal");
 const popupClose = popup.querySelector(".modal__close_times");
 
-if(!(localStorage.getItem("popupClosedBefore")))
-    popup.classList.add("modal_active");
-
 popupClose.addEventListener("click", () => {
-    localStorage.setItem("popupClosedBefore", JSON.stringify(true))
     popup.classList.remove("modal_active");
+})
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log(document.cookie);
+    if(document.cookie.search("modalClosedBefore") != -1) return;
+    document.cookie = "modalClosedBefore=1";
+    popup.classList.add("modal_active");
 });
--- client-state/text-editor/app.js
+++ client-state/text-editor/app.js
@@ -3,20 +3,17 @@ const button = document.getElementById("clear");
 
 function saveText() {
     const text = textField.value;
-    console.log(text);
     localStorage.setItem("text", JSON.stringify(text));
 }
 
 function loadText() {
     const storedText = localStorage.getItem("text");
-
-    if(!storedText) return;
     textField.value = JSON.parse(storedText);
 }
 
 function clearText() {
     textField.value = "";
-    saveText();
+    localStorage.removeItem("text");
 }
 
 loadText();
